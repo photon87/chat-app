81e37c4a098cace22bb5466dd8bd1217682cfa09
photon <andy2887will@gmail.com>
2021-01-24 15:02:03 -0500

    added Conversation Provider and Modal

---
+++ client/src/contexts/ConversationsProvider.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+const ConversationsContext = React.createContext()
+
+export function useConversations() {
+    return useContext(ConversationsContext)
+}
+
+export function ConversationsProvider({ children }) {
+    const [conversations, setConversations] = useLocalStorage('conversations', [])
+
+    function createConversations(recipients){
+        setConversations(prevConversations => {
+            return [...prevConversations, { recipients, messages: [] }]
+        })
+    }
+
+    return (
+        <ConversationsContext.Provider value={{ conversations, createConversations}}>
+            {children}
+        </ConversationsContext.Provider>
+    )
+}
